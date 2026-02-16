eae36f014de029f97cd258ceacf840108039703f
Chris <chrispwong23@gmail.com>
2026-02-15 19:00:48 -0800

    Updated the model

--- Model/unet.py
+++ Model/unet.py
@@ -8,13 +8,9 @@ class DoubleConv(nn.Module):
         super().__init__()
 
         self.conv = nn.Sequential(
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(in_channels, out_channels, 3, padding=0),
-            nn.InstanceNorm2d(out_channels, affine=True),
+            nn.Conv2d(in_channels, out_channels, 3, padding=0, kernel_size=3),
             nn.ReLU(inplace=True),
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(out_channels, out_channels, 3, padding=0),
-            nn.InstanceNorm2d(out_channels, affine=True),
+            nn.Conv2d(out_channels, out_channels, 3, padding=0, kernel_size=3),
             nn.ReLU(inplace=True),
         )
 
@@ -23,7 +19,7 @@ class DoubleConv(nn.Module):
 
 
 class UNetModel(nn.Module):
-    def __init__(self, in_channels=3, out_channels=3):
+    def __init__(self, in_channels=3, num_genres=10):
         super().__init__()
         # Encoder
         self.enc1 = DoubleConv(in_channels, 64)
@@ -36,58 +32,30 @@ class UNetModel(nn.Module):
         # Bottleneck
         self.bridge = DoubleConv(512, 1024)
 
-        # Decoder 
-        self.up0 = nn.Upsample(
-            scale_factor=2, mode='bilinear', align_corners=True)
-        self.up_conv0 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
-        self.dec0 = DoubleConv(1024, 512)
+        # Decoder
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
 
-        self.up1 = nn.Upsample(
-            scale_factor=2, mode='bilinear', align_corners=True)
-        self.up_conv1 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
-        self.dec1 = DoubleConv(512, 256)
-
-        self.up2 = nn.Upsample(
-            scale_factor=2, mode='bilinear', align_corners=True)
-        self.up_conv2 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
-        self.dec2 = DoubleConv(256, 128)
-
-        self.up3 = nn.Upsample(
-            scale_factor=2, mode='bilinear', align_corners=True)
-        self.up_conv3 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-        self.dec3 = DoubleConv(128, 64)
-
-        self.out_layer = nn.Conv2d(64, out_channels, 1)
-
-        nn.init.constant_(self.out_layer.weight, 0)
-        nn.init.constant_(self.out_layer.bias, 0)
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1024, 512),
+            nn.ReLU(True),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_genres)
+        )
 
     def forward(self, x):
-        # Encode
-        e1 = self.enc1(x)
-        e2 = self.enc2(self.pool(e1))
-        e3 = self.enc3(self.pool(e2))
-        e4 = self.enc4(self.pool(e3))
+        x = self.enc1(x)
+        x = self.pool(x)
+        x = self.enc2(x)
+        x = self.pool(x)
+        x = self.enc3(x)
+        x = self.pool(x)
+        x = self.enc4(x)
+        x = self.pool(x)
 
-        # Bottleneck
-        b = self.bridge(self.pool(e4))
+        x = self.bridge(x)
 
-        # Decode
-        d0 = self.up_conv0(self.up0(b))
-        d0 = torch.cat([d0, e4], dim=1)
-        d0 = self.dec0(d0)
+        x = self.gap(x)
+        x = self.classifier(x)
 
-        d1 = self.up_conv1(self.up1(d0))
-        d1 = torch.cat([d1, e3], dim=1)
-        d1 = self.dec1(d1)
-
-        d2 = self.up_conv2(self.up2(d1))
-        d2 = torch.cat([d2, e2], dim=1)
-        d2 = self.dec2(d2)
-
-        d3 = self.up_conv3(self.up3(d2))
-        d3 = torch.cat([d3, e1], dim=1)
-        d3 = self.dec3(d3)
-
-        residual = torch.tanh(self.out_layer(d3))
-        return x + residual
+        return x
